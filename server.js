313762ebd4b7f90ba54126d19a64154b495e768a
Joshua Poveda <joshuapoveda9@gmail.com>
2023-05-11 14:52:48 -0400

    index.jsx view rendered at pokemon route

--- server.js
+++ server.js
@@ -3,10 +3,24 @@ const app = express()
 const pokemon = require("./models/pokemon");
 const port = 3000
 
-app.get('/', (req, res) => {
-    res.send(pokemon)
-  })
+app.set("view engine", "jsx");
+app.engine("jsx", require("jsx-view-engine").createEngine());
+
+
+app.get("/pokemon", (req, res) => {
+    res.render("Index", { pokemon });
+  });
   
   app.listen(port, () => {
     console.log(`listening on port ${port}`)
   })
+
+//INDUCES! Be sure to remember this acronym to keep your routes in working order! When working on a backend application, paste this to help you keep track...
+// Routes...
+// Index : Show all the things! - GET /fruits
+// New : An empty form for a new thing - GET /fruits/new
+// Delete : Get rid of this particular thing! - DELETE /fruits/:id
+// Update : Update this specific thing with this updated form - PUT /fruits/:id
+// Create : Make a new thing with this filled out form - POST /fruits
+// Edit : A prefilled form to update a specific thing - GET /fruits/:id/edit
+// Show : Show me this one thing! - GET /fruits/:id (edited)
